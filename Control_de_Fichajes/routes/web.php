1b38c23a59951d00d0b154943a66849d69eb3b9f
Crisbr10 <borgescristhian1@gmail.com>
2023-07-06 13:03:03 +0200

    Proyecto inicializado

--- Control_de_Fichajes/routes/web.php
+++ Control_de_Fichajes/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdministradorController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
